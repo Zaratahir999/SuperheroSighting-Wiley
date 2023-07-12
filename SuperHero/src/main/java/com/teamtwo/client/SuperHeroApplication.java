b95d867be9d22643fcc8d8e535ef3af1dc0f8e33
RodaAnshur <rodaanshur@icloud.com>
2023-07-12 15:17:04 +0100

    Rodas Location

--- SuperHero/src/main/java/com/teamtwo/client/SuperHeroApplication.java
+++ SuperHero/src/main/java/com/teamtwo/client/SuperHeroApplication.java
@@ -3,7 +3,7 @@ package com.teamtwo.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages ="com.roda")
 public class SuperHeroApplication {
 
 	public static void main(String[] args) {
