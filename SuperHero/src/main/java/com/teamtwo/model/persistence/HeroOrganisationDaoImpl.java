e55192b72ba435424848db70e66906a47c356b1d
Chiara Pigoni <m.chiarapigoni@gmail.com>
2023-07-12 21:05:11 +0100

    Chiara added heroDaoImpl, mapper and tests, HeroOrganisationImpl, mapper and tests and SuperPowerMapper

--- SuperHero/src/main/java/com/teamtwo/model/persistence/HeroOrganisationDaoImpl.java
+++ SuperHero/src/main/java/com/teamtwo/model/persistence/HeroOrganisationDaoImpl.java
@@ -2,20 +2,30 @@ package com.teamtwo.model.persistence;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import com.teamtwo.dto.entity.HeroOrganisation;
 
+
+@Repository
 public class HeroOrganisationDaoImpl implements HeroOrganisationDao {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@Override
 	public List<HeroOrganisation> getAllMembersByOrganisation(int orgId) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "SELECT * FROM HeroOrganisation WHERE orgId = ?";
+		return jdbcTemplate.query(query, new HeroOrganisationRowMapper(), orgId);
 	}
 
 	@Override
 	public List<HeroOrganisation> getOrganisationBySuperHero(int heroId) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "SELECT * FROM HeroOrganisation WHERE heroId = ?";
+		return jdbcTemplate.query(query, new HeroOrganisationRowMapper(), heroId);
 	}
 
 }
+
