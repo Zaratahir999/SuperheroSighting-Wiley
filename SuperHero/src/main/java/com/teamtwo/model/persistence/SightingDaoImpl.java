0ab6be5b21a00ad1590a1f5a9c790b31af38ed28
Zaratahir999 <zara_tahir1999@outlook.com>
2023-07-12 14:55:41 +0100

    SightingsDaoImpl and Mapper updated

--- SuperHero/src/main/java/com/teamtwo/model/persistence/SightingDaoImpl.java
+++ SuperHero/src/main/java/com/teamtwo/model/persistence/SightingDaoImpl.java
@@ -3,50 +3,55 @@ package com.teamtwo.model.persistence;
 import java.sql.Timestamp;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import com.teamtwo.dto.entity.Sighting;
 
+@Repository
 public class SightingDaoImpl implements SightingDao {
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@Override
 	public Sighting getSightingById(int sightingId) {
-		// TODO Auto-generated method stub
-		return null;
+		return jdbcTemplate.queryForObject("SELECT * FROM Sighting WHERE sightingId=?", new SightingRowMapper(), sightingId);
 	}
 
 	@Override
 	public List<Sighting> getAllSighting() {
-		// TODO Auto-generated method stub
-		return null;
+		return jdbcTemplate.query("SELECT * FROM Sighting", new SightingRowMapper());
 	}
 
 	@Override
 	public int editSighting(Sighting sighting) {
-		// TODO Auto-generated method stub
-		return 0;
+		return jdbcTemplate.update("UPDATE Sighting SET heroId = ?, locationId = ?, locationDate = ? WHERE sightingId = ?", sighting.getHeroId(), sighting.getLocationId(), sighting.getLocationDate());
 	}
-
+	
 	@Override
 	public int addSighting(Sighting sighting) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return jdbcTemplate.update("INSERT INTO Sighting VALUES (?, ?, ?, ?) ", sighting.getSightingId(), sighting.getHeroId(), sighting.getLocationId(), sighting.getLocationDate());
 	}
 
 	@Override
 	public int deleteSighting(int sightingId) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return jdbcTemplate.update("DELETE FROM Sighting WHERE sightingId = ?", sightingId);
 	}
 
 	@Override
 	public List<Sighting> getSightingByDate(Timestamp locationDate) {
-		// TODO Auto-generated method stub
-		return null;
+	
+		return jdbcTemplate.query("SELECT * FROM Sighting WHERE locationDate= ? ", new SightingRowMapper(), locationDate);
 	}
 
 	@Override
 	public List<Sighting> getSightingByLocationId(int locationId) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return jdbcTemplate.query("SELECT * FROM Sighting WHERE locationId= ?", new SightingRowMapper(), locationId);
 	}
 
 }
