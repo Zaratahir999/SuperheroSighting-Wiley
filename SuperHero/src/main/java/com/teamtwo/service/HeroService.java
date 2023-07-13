c19b561d7e68189fb55a8b3955e41e72092cf293
Chiara Pigoni <m.chiarapigoni@gmail.com>
2023-07-13 11:58:51 +0100

    Chiara added heroserviceLayeoServiceLayer

---
+++ SuperHero/src/main/java/com/teamtwo/service/HeroService.java
@@ -0,0 +1,16 @@
+package com.teamtwo.service;
+
+import java.util.List;
+
+
+import com.teamtwo.dto.entity.Hero;
+
+public interface HeroService {
+	
+	List<Hero> getAllHeroes();
+	boolean addHero(Hero hero);
+	boolean deleteHeroById(int heroId);
+	List <Hero> getHeroByLocation(int locationId);
+	public Hero editHero(Hero hero);
+
+}
